5b01ed8a4374ced74905d5e8f738403a3e883831
HeeBBang <31871772+HeeBBang@users.noreply.github.com>
2022-02-23 17:56:27 +0900

    # Add functions   - create_block   - get_previous_block   - proof_of_work

--- app.py
+++ app.py
@@ -1,6 +1,37 @@
-from flask import Flask
+import datetime
+import hashlib
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+class Blockchain:
+
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+
+    def create_block(self, proof, previous_hash):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': str(datetime.datetime.now()),
+            'proof': proof,
+            'previous_hash': previous_hash
+        }
+        self.chain.append(block)
+        return block
+
+    def get_previous_block(self):
+        return self.chain[-1]
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof + previous_proof ** 2).encode()).hexdigest()
+
+
+
 
 
 @app.route('/')
